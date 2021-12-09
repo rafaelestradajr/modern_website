3d172a628022bf304d1e6216aca53187bef0d26d
Rafael Estrada <rafaelestrada@comcast.net>
2021-12-08 20:08:50 -0800

    added css

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navbar.css';
+import {RiMenuLine,RiCloseLine} from 'react-icons/ri';
+import logo from '../../assets/logo.svg';
+
+
+const Navbar = () => {
+    return (
+        <div className='gpt3__navbar'>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src={logo} alt='logo'/>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
